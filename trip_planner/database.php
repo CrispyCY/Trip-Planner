ffd7f4e3d731080277e3d27f357bb9c219c7980b
LazyCY <66861325+CrispyCY@users.noreply.github.com>
2021-04-03 17:57:54 +0800

    Add files via upload

--- trip_planner/database.php
+++ trip_planner/database.php
@@ -1,7 +1,7 @@
 <?php
 $db_server = "localhost";
-$db_user = "tp_admin";
-$db_pass = "abcde12345";
+$db_user = "root";
+$db_pass = "";
 $db_name = "trip_planner";
 $con = new mysqli($db_server, $db_user, $db_pass, $db_name);
 if(!$con){
